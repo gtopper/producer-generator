6d941754e8d6429c98587e38650359dbddd0339d
Gal Topper <galt@iguaz.io>
2017-11-14 11:43:20 +0800

    Added print period.

--- src/main/scala/io/iguaz/grab/etl/Main.scala
+++ src/main/scala/io/iguaz/grab/etl/Main.scala
@@ -19,6 +19,7 @@ object Main {
   private val port = config.getInt("port")
   private val table = config.getString("table")
   private val computeParallelism = config.getInt("compute-parallelism")
+  private val printPeriod = config.getInt("print-period")
 
   def main(args: Array[String]): Unit = {
     implicit val actorSystem = ActorSystem()
@@ -27,18 +28,24 @@ object Main {
 
     val headers = List(HttpHeader.parse("X-v3io-function", "PutItem").asInstanceOf[Ok].header)
 
-    val lineSource = Source.fromIterator(() => io.Source.stdin.getLines())
-    val requestSource = lineSource.mapAsyncUnordered(computeParallelism)(line => Future {
-      val (key, json) = Converter(line)
-      HttpRequest(
-        HttpMethods.POST,
-        Uri("http", Authority(Host(host), port), Uri.Path(table) / key),
-        headers,
-        json.toString
-      )
-    })
+    val lineSource = Source.fromIterator(() => io.Source.stdin.getLines()).zipWithIndex.map {
+      case lineAndIndex@(_, i) =>
+        if (i % printPeriod == 0) println(s"at line #$i...")
+        lineAndIndex
+    }
+    val requestSource = lineSource.mapAsyncUnordered(computeParallelism) {
+      case (line, index) => Future {
+        val (key, json) = Converter(line)
+        HttpRequest(
+          HttpMethods.POST,
+          Uri("http", Authority(Host(host), port), Uri.Path(table) / key),
+          headers,
+          json.toString
+        ) -> index
+      }
+    }
     val httpFlow = Http().cachedHostConnectionPool[Long](host, port)
-    val responseSource = requestSource.zipWithIndex.via(httpFlow)
+    val responseSource = requestSource.via(httpFlow)
     responseSource.runWith(Sink.ignore)
   }
 }
