41ce37580499a5b2a8d5bdd3c82753780ef2a304
Gal Topper <galt@iguaz.io>
2017-11-15 12:03:14 +0800

    Use updated_at field instead of artificial timestamp field.

--- src/main/scala/io/iguaz/grab/etl/Converter.scala
+++ src/main/scala/io/iguaz/grab/etl/Converter.scala
@@ -8,10 +8,10 @@ object Converter {
 
   def apply(event: String): (String, JsObject) = {
     val json = Json.parse(event).as[Map[String, JsValue]]
-    val timestamp = json("created_at").as[Long]
     val afterDataString = json("after_data").as[String]
     val afterData = Json.parse(afterDataString).validate[Map[String, Map[String, String]]].get
     val key = afterData("driver_id")("value")
+    val updatedAt = afterData("updated_at")("value")
     val newData = afterData.mapValues { field =>
       val `type` = field("type") match {
         case "int" | "decimal" | "float" => "N"
@@ -22,10 +22,10 @@ object Converter {
         case other => JsString(other)
       }
       Json.obj(`type` -> value)
-    } + ("timestamp" -> Json.obj("N" -> JsString(timestamp.toString)))
+    }
     key -> Json.obj(
       "Item" -> JsObject(newData),
-      "ConditionExpression" -> JsString(s"not(exists(timestamp)) or timestamp < $timestamp")
+      "ConditionExpression" -> JsString(s"""not(exists(updated_at)) or updated_at < "$updatedAt"""")
     )
   }
 }
