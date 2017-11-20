46b11fab688c7c3fde956b801249693d676bb1c7
Gal Topper <galt@iguaz.io>
2017-11-20 17:02:43 +0800

    Change main to Spark job.

--- build.sbt
+++ build.sbt
@@ -12,4 +12,6 @@ scalacOptions += "-target:jvm-1.7"
 
 libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.2.11" % "provided"
 
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.1" % "provided"
+
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
--- src/main/scala/io/iguaz/grab/etl/Main.scala
+++ src/main/scala/io/iguaz/grab/etl/Main.scala
@@ -1,11 +1,18 @@
 package io.iguaz.grab.etl
 
-import scala.io.Source
+import org.apache.spark.sql.SparkSession
 
 object Main {
 
+  private val spark = SparkSession.builder().appName("Grab ETL").getOrCreate()
+
+  import spark.implicits._
+
   def main(args: Array[String]): Unit = {
-    val lineIterator = Source.stdin.getLines()
-    Push(lineIterator)
+
+    val inputPaths = args
+
+    val df = spark.read.load(inputPaths: _*).select("after_data").map(_.getAs[String]("after_data"))
+    df.foreachPartition(Push(_))
   }
 }
