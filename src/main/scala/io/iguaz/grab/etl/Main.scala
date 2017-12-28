ea0b5db7f03efcce4bb111350e3d0523eb932c7d
Gal Topper <galt@iguaz.io>
2017-12-28 11:17:15 +0200

    Changed to new daemon API.

--- src/main/scala/io/iguaz/grab/etl/Main.scala
+++ src/main/scala/io/iguaz/grab/etl/Main.scala
@@ -1,11 +1,9 @@
 package io.iguaz.grab.etl
 
-import java.util.Properties
-
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-import io.iguaz.v3io.container.DoInContainer
+import io.iguaz.v3io.api.container.ContainerID
 import io.iguaz.v3io.kv.KeyValueOperations
 
 object Main {
@@ -13,11 +11,9 @@ object Main {
   def main(args: Array[String]): Unit = {
     val schema = GeneratorSchema.fromResource("/schema.json")
     val updateEntryIterator = Iterator.continually(Generator.generate(schema))
-    updateEntryIterator.foreach(println)
-    DoInContainer(new Properties) { container =>
-      val kvOps = KeyValueOperations(container)
-      val updateFuture = kvOps.updateMultiple(updateEntryIterator)
-      Await.result(updateFuture, Duration.Inf)
-    }
+    //  updateEntryIterator.foreach(println)
+    val kvOps = KeyValueOperations(ContainerID(1), Map.empty[String, Any])
+    val updateFuture = kvOps.updateMultiple(updateEntryIterator)
+    Await.result(updateFuture, Duration.Inf)
   }
 }
