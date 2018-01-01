6c474c6db5da43cdf9bca4509f9f7cd55b25109d
Gal Topper <galt@iguaz.io>
2018-01-01 17:04:16 +0200

    Support specifying container alias on the command line.

--- src/main/scala/io/iguaz/grab/etl/Main.scala
+++ src/main/scala/io/iguaz/grab/etl/Main.scala
@@ -3,7 +3,7 @@ package io.iguaz.grab.etl
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-import io.iguaz.v3io.api.container.ContainerID
+import io.iguaz.v3io.api.container.ContainerAlias
 import io.iguaz.v3io.kv.KeyValueOperations
 
 object Main {
@@ -11,7 +11,8 @@ object Main {
   def main(args: Array[String]): Unit = {
     val schema = GeneratorSchema.fromResource("/schema.json")
     val updateEntryIterator = PrintPeriodIterator.create().map(_ => Generator.generate(schema))
-    val kvOps = KeyValueOperations(ContainerID(1), sys.props.filterKeys(_.startsWith("v3io")).toMap)
+    val containerAlias = ContainerAlias(sys.props.getOrElse("container-alias", "bigdata"))
+    val kvOps = KeyValueOperations(containerAlias, sys.props.filterKeys(_.startsWith("v3io")).toMap)
     val updateFuture = kvOps.updateMultiple(updateEntryIterator)
     Await.result(updateFuture, Duration.Inf)
   }
