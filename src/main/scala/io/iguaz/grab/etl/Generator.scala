f1cfabebe73bbd24a6a2847ebebe5ac6029167c6
Gal Topper <galt@iguaz.io>
2018-01-22 17:03:13 +0800

    Minimal generator for benchmarking.

--- src/main/scala/io/iguaz/grab/etl/Generator.scala
+++ src/main/scala/io/iguaz/grab/etl/Generator.scala
@@ -1,68 +1,26 @@
 package io.iguaz.grab.etl
 
 import java.nio.file.Paths
-import java.util.Random
-
-import scala.collection.breakOut
-
-import org.joda.time.DateTime
-import org.joda.time.format.ISODateTimeFormat
 
 import io.iguaz.grab.etl.Configuration.table
-import io.iguaz.v3io.kv.Filters._
 import io.iguaz.v3io.kv.{OverwriteMode, SimpleRow, UpdateEntry}
 
 object Generator {
 
-  private implicit val formats = org.json4s.DefaultFormats
-
-  private val random = new Random()
-
-  private val valueDomainSize = sys.props.getOrElse("value-domain-size", "1000000").toInt
-
-  private val dateTimeFormatter = ISODateTimeFormat.dateTimeNoMillis()
-
-  private def generateString(): String = random.nextInt(valueDomainSize).toBinaryString
-
-  private def generateFromType(generatorType: String, timestamp: String): Any = generatorType match {
-    case "string" => generateString()
-    case "integer" => random.nextInt(valueDomainSize)
-    case "time" => timestamp
-  }
+  private var count = 0L
 
   def generate(generatorSchema: GeneratorSchema): UpdateEntry = {
 
-    val now = DateTime.now()
-    val timestamp = now.toString(dateTimeFormatter)
+    val path = Paths.get(table)
 
-    val otherFields: Map[String, Any] = generatorSchema.columns.flatMap {
-      case GeneratorField(name, t, nullable) =>
-        if (!nullable | random.nextBoolean()) Some(name -> generateFromType(t, timestamp)) else None
-    }(breakOut)
+    val row = SimpleRow(count.toString, Map("count" -> count))
 
-    val timeFields = List(
-      "year" -> now.getYear,
-      "month" -> now.getMonthOfYear,
-      "day" -> now.getDayOfMonth,
-      "hour" -> now.getHourOfDay
-    )
-    val key = generateFromType(generatorSchema.key.`type`, timestamp)
-    val row = SimpleRow(
-      key.toString,
-      otherFields ++ List(
-        generatorSchema.key.name -> key,
-        generatorSchema.`time-field`.name -> timestamp
-      ) ++ timeFields
-    )
-
-    val subdirs = timeFields.map { case (name, value) => s"$name=$value" }
-    val path = Paths.get(table, subdirs: _*)
+    count += 1
 
     UpdateEntry(
       path,
       row,
-      OverwriteMode.OVERWRITE,
-      Or(Not(Exists(generatorSchema.`time-field`.name)), LessThanOrEqual(generatorSchema.`time-field`.name, timestamp))
+      OverwriteMode.OVERWRITE
     )
   }
 }
